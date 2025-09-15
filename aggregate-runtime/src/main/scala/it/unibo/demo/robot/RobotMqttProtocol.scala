8f96417f46439d3932d3598c0559111241c71c8f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-15 17:50:09 +0200

    fix: set speed

--- aggregate-runtime/src/main/scala/it/unibo/demo/robot/RobotMqttProtocol.scala
+++ aggregate-runtime/src/main/scala/it/unibo/demo/robot/RobotMqttProtocol.scala
@@ -7,20 +7,21 @@ object RobotMqttProtocol:
 
   case class RobotMovement(left: Double, right: Double)
   given RW[RobotMovement] = macroRW
-
+  private val rotationSpeed = 0.6
+  private val forwardSpeed = 1.0
   def spinRight(robot: Int)(using mqttContext: MqttContext) =
-    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(0.4, -0.4)).getBytes, 0, false)
+    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(rotationSpeed, -rotationSpeed)).getBytes, 0, false)
 
   def spinLeft(robot: Int)(using mqttContext: MqttContext): Unit =
-    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(-0.4, 0.4)).getBytes, 0, false)
+    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(-rotationSpeed, rotationSpeed)).getBytes, 0, false)
 
   def nop(robot: Int)(using mqttContext: MqttContext): Unit = ()
   def stop(robot: Int)(using mqttContext: MqttContext): Unit =
     mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(0, 0)).getBytes, 0, false)
   def forward(robot: Int)(using mqttContext: MqttContext): Unit =
-    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(0.9, 0.9)).getBytes, 0, false)
+    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(forwardSpeed, forwardSpeed)).getBytes, 0, false)
 
   def backward(robot: Int)(using mqttContext: MqttContext): Unit =
-    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(-0.9, -0.9)).getBytes, 0, false)
+    mqttContext.client.publish(s"robots/${robot}/move", write(RobotMovement(-forwardSpeed, -forwardSpeed)).getBytes, 0, false)
 
 
