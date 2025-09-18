54d5e614fdde1a0281713c3bdca97bda79d3c45a
RicciNicholas <riccinicho22@gmail.com>
2025-09-18 16:04:42 +0200

    renamed file
    # Conflicts:
    #	dashboard/src/components/Obstacle.tsx

--- dashboard/src/components/Obstacle.tsx
+++ dashboard/src/components/Obstacle.tsx
@@ -5,13 +5,14 @@ import type { ObstacleData } from '../types/ObstacleData';
 
 interface ObstacleProps {
   obstacle: ObstacleData;
+  onClick: () => void;
 }
 
-function Obstacle({ obstacle }: ObstacleProps) {
+function Obstacle({ obstacle, onClick }: ObstacleProps) {
   const meshRef = useRef<THREE.Mesh>(null);
   const { position, size } = obstacle;
   return (
-    <group position={[position.x, 0, position.y]}>
+    <group position={[position.x, 0, position.y]} onClick={onClick}>
       <mesh ref={meshRef} castShadow>
         <boxGeometry args={[size, size, size]} />
         <meshStandardMaterial color="brown" />
