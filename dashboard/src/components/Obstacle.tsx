2603125179f9d53d7772066ef7f776e1d9f9216a
RicciNicholas <riccinicho22@gmail.com>
2025-09-18 16:35:23 +0200

    removed for now onClick

--- dashboard/src/components/Obstacle.tsx
+++ dashboard/src/components/Obstacle.tsx
@@ -5,14 +5,13 @@ import type { ObstacleData } from '../types/ObstacleData';
 
 interface ObstacleProps {
   obstacle: ObstacleData;
-  onClick: () => void;
 }
 
-function Obstacle({ obstacle, onClick }: ObstacleProps) {
+function Obstacle({ obstacle }: ObstacleProps) {
   const meshRef = useRef<THREE.Mesh>(null);
   const { position, size } = obstacle;
   return (
-    <group position={[position.x, 0, position.y]} onClick={onClick}>
+    <group position={[position.x, 0, position.y]}>
       <mesh ref={meshRef} castShadow>
         <boxGeometry args={[size, size, size]} />
         <meshStandardMaterial color="brown" />
