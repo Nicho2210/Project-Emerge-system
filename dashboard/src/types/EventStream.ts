06a164c2783ff52b3000d403b556fd07b45461e4
RicciNicholas <riccinicho22@gmail.com>
2025-09-19 09:39:52 +0200

    added callback for obstacle in EventStream.ts

--- dashboard/src/types/EventStream.ts
+++ dashboard/src/types/EventStream.ts
@@ -1,6 +1,7 @@
 import type { RobotData } from './RobotData';
+import type { ObstacleData } from './ObstacleData';
 
 export interface EventStream {
-  subscribe(callback: (robots: RobotData[]) => void): void;
+  subscribe(callback: (robots: RobotData[], obstacles: ObstacleData[]) => void): void;
   cleanup(): void;
 }
