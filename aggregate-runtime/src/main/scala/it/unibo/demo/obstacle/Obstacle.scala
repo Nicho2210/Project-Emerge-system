a2c235171537a816797cbb762260f6dcafe69754
RicciNicholas <riccinicho22@gmail.com>
2025-09-19 09:40:51 +0200

    deleted Obstacle.scala

--- aggregate-runtime/src/main/scala/it/unibo/demo/obstacle/Obstacle.scala
+++
@@ -1,7 +0,0 @@
-package it.unibo.demo.obstacle
-
-import upickle.default.{macroRW, ReadWriter as RW, *}
-
-case class Obstacle(id: String, x: Double, y: Double, size: Double)
-
-given RW[Obstacle] = macroRW
