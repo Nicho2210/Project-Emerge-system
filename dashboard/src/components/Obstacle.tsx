9b717f53e0e5af1f6d880421cba63c3bda9f68fc
RicciNicholas <riccinicho22@gmail.com>
2025-09-22 17:18:28 +0200

    modified obstacle mesh

--- dashboard/src/components/Obstacle.tsx
+++ dashboard/src/components/Obstacle.tsx
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { useRef } from 'react';
-import { useGLTF } from '@react-three/drei';
 import type { ObstacleData } from '../types/ObstacleData';
 
 const greenMaterial = new THREE.MeshStandardMaterial({ color: 'green' });
@@ -11,29 +10,17 @@ interface ObstacleProps {
 
 function Obstacle({ obstacle }: ObstacleProps) {
   const meshRef = useRef<THREE.Mesh>(null);
-  const { position } = obstacle;
-  const { nodes } = useGLTF('/src/assets/robot-90.gltf') as any; 
+  const { position, size } = obstacle;
 
   return (
-    <group 
-      receiveShadow
-      position={[position.x, 0.017, position.y]} 
-      ref={meshRef}
-      scale={[0.001, 0.001, 0.001]} 
-    >
-      <mesh
-        geometry={nodes.robot_1.geometry}
-        material={greenMaterial}
-      />
-      <mesh
-        geometry={nodes.robot_2.geometry}
-        material={greenMaterial}
-      />
-      <mesh
-        geometry={nodes.robot.geometry}
-        material={greenMaterial}
-      />
-    </group>
+    <mesh
+          receiveShadow
+          position={[position.x, 0.017, position.y]}
+          ref={meshRef}
+          material={greenMaterial}
+        >
+            <boxGeometry args={[size, size, size]} />
+        </mesh>
   );
 }
 
